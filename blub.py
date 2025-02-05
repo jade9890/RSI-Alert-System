5146e08fd0fda8abcfa986bc3c38beed5d381252
jade9890 <jadeluong103@yahoo.ca>
2025-02-05 14:09:24 -0500

    added more intervals

--- blub.py
+++ blub.py
@@ -6,26 +6,38 @@ import matplotlib.pyplot as plt
 # list of stocks 
 tickers_list = ["SOXL", "SOXS", "UBER", "PDD", "JKS", "NVDA", "MU", "MSFT"]
 
-#download the data of all stocks, 100 candles
-data = yf.download(tickers_list, period="1mo", interval="1h", group_by='ticker')
+#download stock data in 1hr, 3min, and 4hr intervals
+oneHourData = yf.download(tickers_list, period="1mo", interval="1h", group_by='ticker')
+fiveMinData = yf.download(tickers_list, period="1mo", interval="5m", group_by='ticker')
+ninetyMinData = yf.download(tickers_list, period="1mo", interval="90m", group_by='ticker')
+
+oneHourData.index = oneHourData.index.tz_localize(None)
+fiveMinData.index = fiveMinData.index.tz_localize(None)
+ninetyMinData.index = ninetyMinData.index.tz_localize(None)
 
 
-close_prices = data.xs("Close", axis=1, level=1)
+close_prices_5min = fiveMinData.xs("Close", axis=1, level=1)
+close_prices_hour = oneHourData.xs("Close", axis=1, level=1)
+close_prices_90min = ninetyMinData.xs("Close", axis=1, level=1)
 
+timeframes = {
+    "1h": close_prices_hour,
+    "5m": close_prices_5min,
+    "90m": close_prices_90min
+}
 #dictionary to put data in
 rsi_data={}
 
 #loop through each ticker and get the RSI
-for ticker in tickers_list:
-    if ticker in close_prices:
-        rsi_data[ticker] = ta.RSI(close_prices[ticker], timeperiod=14)
+for timeframe, close_price_df in timeframes.items():
+    for ticker in tickers_list:
+        if ticker in close_price_df.columns:
+            rsi_data[(ticker, timeframe)] = ta.RSI(close_price_df[ticker], timeperiod=14)
 
 rsi_dataframe=pd.DataFrame(rsi_data)
-print(rsi_dataframe.tail(1))
+print(rsi_dataframe.tail(10))
 
-#def generate_rsi_signals(data, period=14, overbough_threshold=70,oversolv_threshold=30):
-    #calculate RSI
-    #initialize signal column with neutral
-    #data['Signal'] = 'Neutral'
+
+
 
     
